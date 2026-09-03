2e226da9aa5c553a180e0cda34e36f27027e66f2
Simon J Stuart <lakraven@gmail.com>
2026-09-03 19:52:46 +0200

    Expose Mesh membership retention coordination

--- src/ESPressio_Mesh.hpp
+++ src/ESPressio_Mesh.hpp
@@ -8,3 +8,4 @@
 #include "ESPressio_InboundDeliveryReservations.hpp"
 #include "ESPressio_AuthenticatedMembershipTable.hpp"
 #include "ESPressio_InboundDeliveryCoordinator.hpp"
+#include "ESPressio_MembershipRetentionCoordinator.hpp"
