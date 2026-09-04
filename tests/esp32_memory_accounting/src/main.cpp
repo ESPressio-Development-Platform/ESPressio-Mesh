2eb1993797748fca10f333e64439eef0fdff014c
Simon J Stuart <lakraven@gmail.com>
2026-09-04 16:40:47 +0200

    Retain ESP32 accounting probe symbols

--- tests/esp32_memory_accounting/src/main.cpp
+++ tests/esp32_memory_accounting/src/main.cpp
@@ -12,6 +12,7 @@ struct TestTopologyCharacteristics final {
 };
 
 using Accounting = MeshFixedMemoryAccounting<TestTopologyCharacteristics>;
+volatile std::uint8_t espressio_mesh_probe_sink = 0;
 }
 
 extern "C" {
@@ -30,5 +31,21 @@ __attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_traffic_go
 __attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_ack_tracker_8[Accounting::DeliveryAcknowledgementBytes<8>()];
 }
 
-void setup() {}
+void setup() {
+    // Volatile reads make every probe array a live linker dependency so the Xtensa ELF retains its exact ABI-sized symbol.
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_authenticated_membership[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_membership_liveness[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_membership_tombstones[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_inbound_delivery_reservations[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_pending_neighbour_candidates[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_inbound_authentication_reservations[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_liveness_probe_reservations[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_authenticated_direct_peer_bindings[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_topology_graph[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_route_cache[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_primitive_receiver_registry[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_traffic_governor[0];
+    espressio_mesh_probe_sink ^= espressio_mesh_accounting_ack_tracker_8[0];
+}
+
 void loop() {}
