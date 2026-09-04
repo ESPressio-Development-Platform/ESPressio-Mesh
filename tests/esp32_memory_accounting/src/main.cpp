27415ac193232958758da48865293962ece0c67d
Simon J Stuart <lakraven@gmail.com>
2026-09-04 16:36:18 +0200

    Expose ESP32-native Mesh accounting symbols

---
+++ tests/esp32_memory_accounting/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <cstdint>
+
+#include <ESPressio_MeshMemoryAccounting.hpp>
+
+using namespace ESPressio::Mesh;
+
+namespace {
+struct TestTopologyCharacteristics final {
+    std::int16_t SignalDbm{0};
+    std::uint16_t CostHint{0};
+};
+
+using Accounting = MeshFixedMemoryAccounting<TestTopologyCharacteristics>;
+}
+
+extern "C" {
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_authenticated_membership[Accounting::AuthenticatedMembershipBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_membership_liveness[Accounting::MembershipLivenessBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_membership_tombstones[Accounting::MembershipTombstoneBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_inbound_delivery_reservations[Accounting::InboundDeliveryReservationBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_pending_neighbour_candidates[Accounting::PendingNeighbourCandidateBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_inbound_authentication_reservations[Accounting::InboundAuthenticationReservationBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_liveness_probe_reservations[Accounting::LivenessProbeReservationBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_authenticated_direct_peer_bindings[Accounting::AuthenticatedDirectPeerBindingBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_topology_graph[Accounting::TopologyGraphBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_route_cache[Accounting::RouteCacheBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_primitive_receiver_registry[Accounting::PrimitiveReceiverRegistryBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_traffic_governor[Accounting::TrafficGovernorBytes];
+__attribute__((used)) volatile std::uint8_t espressio_mesh_accounting_ack_tracker_8[Accounting::DeliveryAcknowledgementBytes<8>()];
+}
+
+void setup() {}
+void loop() {}
